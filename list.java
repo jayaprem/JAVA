c87836bfefc3947156b3d0d54947da470fb44d52
jayaprem <36623938+jayaprem@users.noreply.github.com>
2018-06-18 14:46:37 +0530

    Create list.java

---
+++ list.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String sc="jayalakshmi";
+		char[] c=sc.toCharArray();
+		int l=sc.length();
+		for(int j=0;i<=l;i++)
+		{
+			if(j%2==0)
+			{
+				System.out.println(c[j]);
+			}
+		}
+	}
+}
