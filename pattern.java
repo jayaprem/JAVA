740c157b10b5a090b8284a9f7ac38530c30d5b89
jayaprem <36623938+jayaprem@users.noreply.github.com>
2018-06-19 14:21:05 +0000

    Create pattern.java

---
+++ pattern.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int i,j,t=1;
+	for(i=1;i<9;i++)
+	{ 
+             for(j=1;j<i;j++)
+             {
+             System.out.print(t+"\t");
+             t++;
+             }
+             System.out.print("\n");
+	}
+	}
+}
+1
+2 3
+4 5 6
+7 8 9 10
