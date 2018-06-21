9fb59cd25b1b6e3226bb10598a525c5eaeb1e3ac
jayaprem <36623938+jayaprem@users.noreply.github.com>
2018-06-21 14:00:11 +0530

    Create skipping2numbers.java

---
+++ skipping2numbers.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int count=0;
+	for(int i=100;i>=1;i--)
+	{
+		if(count%3==0)
+		{
+			System.out.println(i);
+		}
+		count++;
+	}
+	
+	}
+}100 97 94 91 88
