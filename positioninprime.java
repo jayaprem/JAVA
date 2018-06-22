e8c86a2f7d60b74871331dab4ab5fd72883ba77e
jayaprem <36623938+jayaprem@users.noreply.github.com>
2018-06-22 11:49:18 +0530

    Update positioninprime.java

--- positioninprime.java
+++ positioninprime.java
@@ -1,25 +1,44 @@
+import java.util.*;
+import java.lang.*;
 import java.io.*;
-import  java.util.*;
-
-class UserMainCode
+class Ideone
 {
 
-    public static int output1;
+		public static int output1;
 	public static int a=0;
 	public static int b=1;
 	    public static int count=1;
 		
     public static void nthFibonacci(int input1){
+    	if(input1==0)
+    	{
+    		output1=0;
+    		System.out.println(output1);
+    	}
+    	else if(input1==1)
+    	{
+    		output1=1;
+    		System.out.println(output1);
+    	}
+    	else
+    	{
 		for(int i=2;i<=input1;i++)
 		{
 		output1=a+b;
-        a=b;
+                a=b;
 		b=output1;
-			count++;
+		count++;
 			if(count==input1)
 			{
 				System.out.println(output1);
 			}
+		}
+    	}
     }
+				public static void main (String[] args) throws java.lang.Exception
+	{
+		Ideone i=new Ideone();
+		i.nthFibonacci(5);
+
 	}
 }
