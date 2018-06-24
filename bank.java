a112342b7865a806adfde4cd7dfa0d8d0d344551
jayaprem <36623938+jayaprem@users.noreply.github.com>
2018-06-24 11:26:58 +0000

    Update bank.java

--- bank.java
+++ bank.java
@@ -1,53 +1,71 @@
-int accno,,dateopen,branchcode,accbalance;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+int accno,dateopen,branchcode,accbalance;
 String acctype,accholder;
 int count=0;
 int month=1;
-bank(int ano,String atype,String aholder,int dateopen,int bcode,int balance)
+Ideone(int ano,String atype,String aholder,int balance)
 {
 accno=ano;
 acctype=atype;
 accholder=aholder;
-dateopen=dateopen;
-branchcode=bcode;
+/*dateopen=dateopen;
+branchcode=bcode;*/
 accbalance=balance;
 }
 public void deposit(int amt)
 {
 accbalance=accbalance+amt;
-Sytem.out.println("deposited amt is"+amt);
-Sytem.out.println("accoun balance is"+accbalance);
+System.out.println("deposited amt is"+amt);
+System.out.println("accoun balance is"+accbalance);
 count=count+1;
 }
 public void withdraw(int amt)
 {
-accbalance=accbalance-amt;
-Sytem.out.println("deposited amt is"+amt);
-Sytem.out.println("accoun balance is"+accbalance);
-count=count+1;
-}
-if(count>3 && month<31)
-{
-accbalance=accbalance-50;
-System.out.println("fine amt 50 is deducted from your account your current balance is"+accbalance);
-}
 if(acctype=="saving")
 {
-System.out.println("your limit per day is 50000");
+
 if(amt>50000)
 {
+	System.out.println("your limit per day is 50000");
 accbalance=accbalance-100;
-System.out.println(accbalance);
-System.out.println("fine amt "100 deducted from your account and your cuurent balace"+accbalace);.
-}
+accbalance=accbalance-amt;
+System.out.println("fine amt 100 deducted from your account becoz you withdraw"+amt);
+System.out.println("current balance"+accbalance);
 }
 else
 {
+accbalance=accbalance-amt;
+System.out.println("withdrawn amount is"+amt);
+System.out.println("account balance"+accbalance);	
+}
+}
+else 
+{
 
-System.out.println("your limit per day is 50000");
 if(amt>100000)
 {
+	System.out.println("your limit per day is 100000");
 accbalance=accbalance-100;
-System.out.println(accbalance);
-System.out.println("fine amt "100 deducted from your account and your cuurent balace"+accbalace);.
+accbalance=accbalance-amt;
+System.out.println("fine amt 100 deducted from your account becoz you withdraw"+amt);
+System.out.println("current balance"+accbalance);
+}
+else
+{
+	accbalance=accbalance-amt;
+	System.out.println("withdrawn amount is"+amt);
+System.out.println("acc balance"+accbalance);
 }
 }
+}
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Ideone i=new Ideone(1,"current","jaya",200000);
+		i.withdraw(50000);
+	}
+}
+	
