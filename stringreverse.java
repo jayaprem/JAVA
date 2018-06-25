44a02a7b673a25bd29490397886091683d4bd9c0
jayaprem <36623938+jayaprem@users.noreply.github.com>
2018-06-25 11:01:22 +0530

    Create stringreverse.java

---
+++ stringreverse.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String s="jayalakshmi";
+		StringBuilder s1=new StringBuilder();
+		s1.append(s);
+		System.out.println(s1.reverse());
+	}
+}
