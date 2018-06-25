d1857babbbfb135ed0433f69ece741f3f916f121
jayaprem <36623938+jayaprem@users.noreply.github.com>
2018-06-25 15:00:05 +0530

    Update String.java

--- String.java
+++ String.java
@@ -12,7 +12,9 @@ class Ideone
 		String[] c=s2.split(" ");
 		String[] a=s1.split(" ");
 		String[] b=new String[a[t++].length()];
-	
+	           StringBuilder n1=new StringBuilder();
+	           n1.append(s1);
+	           System.out.println(n1.reverse());
 		for(int i=0;i<a.length;i++)
 		{
 			String y1=a[i].substring(1);
@@ -20,26 +22,36 @@ class Ideone
 			char j=Character.toUpperCase(x);
 		             b[i]=j+y1;
 			System.out.print(b[i]+"\t");
-			/*s.add(b[i]);*/
 		}
 		System.out.print("\n");
 		for(int j=a.length-1;j>=0;j--)
 		{
+			char n;
+			
 			int l=b[j].length();
 			for(int k=l-1;k>=0;k--)
 			{
-			System.out.print(b[j].charAt(k));
-			}
+			 n=b[j].charAt(k);
+			 System.out.print(n);
+			
+	                        }
+			System.out.print("\t");
+			
 		}
 		System.out.print("\n");
             	for(int i=0;i<c.length;i++)
 		{
+			
 			String y1=c[i].substring(1);
 			char x=a[i].charAt(0);
 			char j=Character.toLowerCase(x);
 		             b[i]=j+y1;
 			System.out.print(b[i]+"\t");
 		}
-	
 	}
 }
+WIPRO TECHNOLOGIES BANGALURU
+urulagnab seigolonhcet orpiw
+Wipro	Technologies	Bangaluru	
+urulagnaB	seigolonhceT	orpiW	
+wIPRO	tECHNOLOGIES	bANGALURU	
