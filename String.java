acf9968cf8d234b1ea9ca4bb97ad7c8882da3e8f
jayaprem <36623938+jayaprem@users.noreply.github.com>
2018-06-25 15:53:49 +0530

    Update String.java

--- String.java
+++ String.java
@@ -7,14 +7,27 @@ class Ideone
 	{
 		int t=0;
 		String s1="wipro technologies bangaluru";
+		System.out.println("GIVEN INPUT-"+s1);
 		String s2=s1.toUpperCase();
-		System.out.println(s2);
+		System.out.println("UPPER CASE OF INPUT-"+s2);
 		String[] c=s2.split(" ");
 		String[] a=s1.split(" ");
 		String[] b=new String[a[t++].length()];
 	           StringBuilder n1=new StringBuilder();
 	           n1.append(s1);
-	           System.out.println(n1.reverse());
+	           System.out.println("REVERSE OF INPUT-"+n1.reverse());
+	           String n3=n1.toString();
+	           String[] z=n3.split(" ");
+	           System.out.println("CAMELCASE - REVERSE STRING:");
+	           for(int i=0;i<z.length;i++)
+		{
+			String y1=z[i].substring(1);
+			char x=z[i].charAt(0);
+			char j=Character.toUpperCase(x);
+			System.out.print(j+y1+"\t");
+		}
+		System.out.print("\n");
+		System.out.println("CAMELCASE - GIVEN STRING:");
 		for(int i=0;i<a.length;i++)
 		{
 			String y1=a[i].substring(1);
@@ -24,21 +37,14 @@ class Ideone
 			System.out.print(b[i]+"\t");
 		}
 		System.out.print("\n");
-		for(int j=a.length-1;j>=0;j--)
-		{
-			char n;
-			
-			int l=b[j].length();
-			for(int k=l-1;k>=0;k--)
-			{
-			 n=b[j].charAt(k);
-			 System.out.print(n);
-			
-	                        }
-			System.out.print("\t");
-			
-		}
+		System.out.println("REVERSE:");
+		for(int j=a.length-1;j>=0;j--){
+			StringBuilder n2=new StringBuilder();
+ n2.append(b[j]);
+	           System.out.print(n2.reverse()+"\t");
+	           }
 		System.out.print("\n");
+		System.out.println("ONLY FISRT LETTER SMALL:");
             	for(int i=0;i<c.length;i++)
 		{
 			
@@ -50,8 +56,15 @@ class Ideone
 		}
 	}
 }
-WIPRO TECHNOLOGIES BANGALURU
-urulagnab seigolonhcet orpiw
-Wipro	Technologies	Bangaluru	
-urulagnaB	seigolonhceT	orpiW	
-wIPRO	tECHNOLOGIES	bANGALURU	
+    GIVEN INPUT-wipro technologies bangaluru
+    UPPER CASE OF INPUT-WIPRO TECHNOLOGIES BANGALURU
+    REVERSE OF INPUT-urulagnab seigolonhcet orpiw
+    CAMELCASE - REVERSE STRING:
+    Urulagnab	Seigolonhcet	Orpiw	
+    CAMELCASE - GIVEN STRING:
+    Wipro	Technologies	Bangaluru	
+    REVERSE:
+    urulagnaB	seigolonhceT	orpiW	
+    ONLY FISRT LETTER SMALL:
+    wIPRO	tECHNOLOGIES	bANGALURU	
+
