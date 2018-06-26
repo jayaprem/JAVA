8f5032f751f473ed80075f71aa44be4464150d2b
jayaprem <36623938+jayaprem@users.noreply.github.com>
2018-06-26 15:32:54 +0530

    Update and rename doubt.java to password.java

--- password.java
+++ password.java
@@ -3,35 +3,27 @@ import java.lang.*;
 import java.io.*;
 class Ideone
 {
-
-			int a;
-			int[] k=new int[4];
-			int[] l=new int[4];
-			int[] p=new int[4];
-			int[] q=new int[4];
-			int[] r=new int[4];
-			int[] s=new int[4];
-			public  void findPass(int input1,int input2,int input3,int input4)
-			{
+int a;
+int[] k=new int[4];
+int[] l=new int[4];
+int[] p=new int[4];
+int[] q=new int[4];
+int[] r=new int[4];
+int[] s=new int[4];
+public  void findPass(int input1,int input2,int input3,int input4)
+{
 				int x[]={input1,input2,input3,input4};
 			for(int i=0;i<x.length;i++)
 			{
 					int count=0;
-					int large=0;
 			while(x[i]>0)
 			{
 			 a=x[i]%10;
-			/* if(a>=large)
-		            {
-			large=a;
-			k[i]=large;
-			
-                                    }*/
-                                  count++;
+                                     count++;
 			if(count==1)
 			{
 				 p[i]=a;
-				
+				 
 			}
 			else if(count==2)
 			{
@@ -48,20 +40,13 @@ class Ideone
 			x[i]=x[i]/10;
 			}
 			}
-			for(int i=0;i<4;i++)
-			{
-				System.out.println(p[i]);
-			}
-			for(int i=0;i<4;i++)
-			{
-				int l=0;
-				if(p[i]>=l)
-				{
-					p[i]=l;
-				}
-			}
-			
-			}
+			Arrays.sort(p);
+			Arrays.sort(q);
+			Arrays.sort(r);
+			Arrays.sort(s);
+			System.out.println("password is:");
+			System.out.println(s[3]+"\t"+r[3]+"\t"+q[3]+"\t"+p[3]);
+}
 	public static void main (String[] args) throws java.lang.Exception
 	{
             Ideone i=new Ideone();
