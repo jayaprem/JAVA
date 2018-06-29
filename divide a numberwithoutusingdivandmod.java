92ce1510c9dcef267ee21926388ab06d49cf0d09
jayaprem <36623938+jayaprem@users.noreply.github.com>
2018-06-29 12:16:53 +0530

    Update divide a numberwithoutusingdivandmod.java

--- divide a numberwithoutusingdivandmod.java
+++ divide a numberwithoutusingdivandmod.java
@@ -29,3 +29,19 @@ int q=0;
 	}
 
 }
+You are given an n-digit large number, you have to check whether it is divisible by 999 without dividing or finding modulo of number by 999.
+Input:
+The first line of input contains an integer T denoting the number of test cases. Then T test cases follow. Each test case contains an string denoting the number.
+
+Output:
+Print "Divisible" (without quotes) if the number is divisible by 999.
+Print "Not divisible" (without quotes) if the number is not divisible by 999.
+
+Example: Input: 2
+1998
+999999999
+
+Output: 
+Divisible
+Not divisible
+
