6cd29810da6fd0baed0ecbe33a04f553d6f1e3ad
jayaprem <36623938+jayaprem@users.noreply.github.com>
2018-07-13 14:01:17 +0000

    Create pyramid.java

---
+++ pyramid.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int count=1;
+		for(int i=n;i>0;i--)
+		{
+			for(int j=1;j<i;j++)
+			{
+				System.out.print(" ");
+			}
+			for(int k=0;k<count;k++)
+			{
+				System.out.print("*");
+			}
+			count=count+2;
+			System.out.print("\n");
+		}
+	}
+}
