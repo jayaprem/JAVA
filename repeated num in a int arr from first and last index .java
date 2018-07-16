cb9f38472adaab6d5f79cca82aa39b223706dfd4
jayaprem <36623938+jayaprem@users.noreply.github.com>
2018-07-16 14:18:57 +0000

    Create repeated num in a int arr from first and last index .java

---
+++ repeated num in a int arr from first and last index .java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int[] a={1,2,8,1,2,4,5,3,1,5,5,3,4,5};
+		Set s1=new LinkedHashSet();
+		for(int i=0;i<a.length;i++)
+		{
+			int count=1;
+			for(int j=i+1;j<a.length;j++)
+			{
+				if(a[i]==a[j])
+				{
+				count++;	
+				}
+			}
+			if(count>1)
+			{
+					s1.add(a[i]);
+			}
+		}
+		System.out.println(s1);
+		
+		
+		ArrayList<Integer>  al=new ArrayList<Integer>(s1);
+			System.out.println(al.get(0));//first index
+			System.out.println(al.get(al.size()-1));//last index
+		
+	}
+}
