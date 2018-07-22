7e3fa02a3c49d528a7cacea2c50f50486a9d251a
jayaprem <36623938+jayaprem@users.noreply.github.com>
2018-07-22 21:05:49 +0530

    Update lab.java

--- lab.java
+++ lab.java
@@ -1,9 +1,16 @@
+import java.io.*;
+import java.util.*;
+class caesar
+{
+public static void main(String[] args)
+{
 Scanner sc=new Scanner(System.in);
+System.out.println("ENCRYPTION:");
 		String s1=sc.nextLine();
 		int k=sc.nextInt();
 		String s=s1.toUpperCase();
 		String[] a=s.split(" ");
-		String b="";
+		//String b="";
 		int cj;
 		System.out.println("CYPER TEXT:");
 		for(int i=0;i<a.length;i++)
@@ -14,24 +21,30 @@ Scanner sc=new Scanner(System.in);
 				int c1=k+(int)c[j];
 				if(c1<=90)
 				{
-				b=b+(char)c1;
+				//b=b+(char)c1;
 				System.out.print((char)c1);
 				}
 				if(c1>90)
 				{
 				cj=c1-26;
-				b=b+(char)cj;
+				//b=b+(char)cj;
 				System.out.println((char)cj);
 				}
 			}
-			b=b+" ";
+			//b=b+" ";
 			System.out.print(" ");
 		}
-		String[] b1=b.split(" ");
+		//String[] b1=b.split(" ");
+		System.out.println("\nDECRYPTION:");
+		Scanner sc1=new Scanner(System.in);
+		String sj=sc1.nextLine();
+		String s2=sj.toUpperCase();
+		String[] s3=s2.split(" ");
+		int k2=sc1.nextInt();
 		System.out.println("\nPLAIN TEXT:");
-		for(int i=0;i<b1.length;i++)
+		for(int i=0;i<s3.length;i++)
 		{
-		char[] c2=b1[i].toCharArray();
+		char[] c2=s3[i].toCharArray();
 			for(int j=0;j<c2.length;j++)
 			{
 				int c3=(int)c2[j]-k;
@@ -47,4 +60,5 @@ Scanner sc=new Scanner(System.in);
 			}
 			System.out.print(" ");
 		}	
-
+}
+}
