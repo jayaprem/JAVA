96e1a5661bd1c29174874b96707d6b007afc97f6
jayaprem <36623938+jayaprem@users.noreply.github.com>
2018-08-09 20:40:41 +0530

    Update nambiar.java

--- nambiar.java
+++ nambiar.java
@@ -1,52 +1,59 @@
-/* package whatever; // don't place package name! */
+QUES:
+mechanism to process any given mobile no and thus generate new resultant no.the mechanism is as follows:
+In the given mobile number starting with the 1st digit keep on adding the subsequent digit till the state (even or odd of the sum of the digit) is opposite to the state(odd or evenof the 1st digit).
+continue this from the subsequent digit till the last digit of the mobile number is reached.
+concatenating the sum
+
+EXAMPLE:
+9880127431
+9-odd=>9+8+8+0+1=26(even digit)
+2-even=>2+7=9(odd)
+4-even=>4+3=7(odd)
+remaining =>1
+output:
+26971(concate)
 
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
-class Ideone
+class N
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		int[] a={9,8,8,0,1,2,7,4,3,1};
-		int i=0,j,sum=0,t;
-		String s="";
-		t=i;
-		while(t<=a.length)
+		int i=0,sum=0;
+		String s=" ";
+		while(i<a.length)
 		{
 			if(a[i]%2!=0)
 			{
-				while(sum%2==0)
+			
+				do
 				{
-				for(j=0;j<a.length-1;j++)
-                                                {
-				sum=sum+a[j];
+				sum=sum+a[i];
 				i++;
-                                                }
-				}
-				t=i;
+				}while(sum%2!=0);
 				s=s+sum;
 				sum=0;
-				a[i]=a[t];
-				
 			}
 			else if(a[i]%2==0)
 			{
-				while(sum%2!=0)
+				do
 				{
-					for(i=0;i<a.length;i++)
-                                                            {
 					sum=sum+a[i];
 					i++;
-                                                            }
-				}
-				t=i;
+				}while(sum%2==0);
 				s=s+sum;
 				sum=0;
-				a[i]=a[t];
 			}
+			        if(i==a.length-1)
+				{
+				s=s+a[i];
+	      break;
+				}
+			
 		}
+			
 		System.out.println(s);
 	}
 }
