483f76b4cb785fc7f7d59a32e4a5cce9c87d4868
jayaprem <36623938+jayaprem@users.noreply.github.com>
2018-10-27 19:44:28 +0530

    Create remove vowels from a string.java

---
+++ remove vowels from a string.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	String s="jayalakshmiaeiou";
+String g=s.replaceAll("[aeiouAEIOU]","");
+System.out.print(g);
+	}
+}
