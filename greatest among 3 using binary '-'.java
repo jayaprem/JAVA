24745ec5b966cf42f2ff18560c3ebed615d070de
jayaprem <36623938+jayaprem@users.noreply.github.com>
2018-12-04 12:56:37 +0530

    Create greatest among 3 using binary '-'.java

---
+++ greatest among 3 using binary '-'.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int c=sc.nextInt();
+    if(a-b>0 && a-c>0)
+         System.out.println("Greatest is a"+a);
+    else if(b-c>0)
+              System.out.println("Greatest is b"+b);
+         else
+             System.out.println("Greatest is c"+c);
+	}
+}
