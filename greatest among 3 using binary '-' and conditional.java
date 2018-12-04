c8d8f8cdc6b7a17a5658e43d31daf518cb3b6559
jayaprem <36623938+jayaprem@users.noreply.github.com>
2018-12-04 13:04:28 +0530

    Update and rename greatest among 3 using binary '-'.java to greatest among 3 using binary '-' and conditional.java

--- greatest among 3 using binary '-' and conditional.java
+++ greatest among 3 using binary '-' and conditional.java
@@ -19,3 +19,18 @@ class Ideone
              System.out.println("Greatest is c"+c);
 	}
 }
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int c=sc.nextInt();
+    int large=(a>b&&a>c?a:b>c?b:c);
+System.out.println(large);
+}
+}
