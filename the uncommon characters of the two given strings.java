dc770ad9fa0532c5d30a2ecc1e73222a311ea175
jayaprem <36623938+jayaprem@users.noreply.github.com>
2018-12-29 06:50:25 +0530

    Update the uncommon characters of the two given strings.java

--- the uncommon characters of the two given strings.java
+++ the uncommon characters of the two given strings.java
@@ -90,4 +90,36 @@ Input:
 characters
 alphabets
 Output: bclpr
+USING STRING:
+/* package whatever; // don't place package name! */
 
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String k="characters";
+		String k1="alphabets";
+		String s="";
+		int i,j;
+	for(i=0;i<k.length();i++)
+	{
+		for(j=0;j<k1.length();j++)
+		{
+			if(k.charAt(i)==k1.charAt(j)) s=s+k.charAt(i);
+		}
+	}
+	for(i=0;i<s.length();i++)
+	{
+		String g=Character.toString(s.charAt(i));
+	k=k.replaceAll(g,"");
+	k1=k1.replaceAll(g,"");
+	}
+	System.out.println(k+""+k1);
+}
+}
+output:crcrlpb
